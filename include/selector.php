a91554b2912d7fcb28a222cf31ddb296fb82f9c4
Gogs <gogs@fake.local>
2016-12-11 07:20:16 +1100

    x

--- include/selector.php
+++ include/selector.php
@@ -10,24 +10,34 @@ Date: 2016
 $ini_file = '/etc/config/cotd.properties';                                                                                                               
 
 // Test if OpenShift Online V2
-$db = getenv('OPENSHIFT_MYSQL_DB_HOST');
-                                                                                                                                                            
+if ( !empty(getenv('OPENSHIFT_MYSQL_DB_HOST')) ) {
+    $_SESSION['DBHOST'] = getenv('OPENSHIFT_MYSQL_DB_HOST');
+    $_SESSION['DBPORT'] = getenv('OPENSHIFT_MYSQL_DB_PORT');
+    $_SESSION['DBUSER'] = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+    $_SESSION['DBPASSWORD'] = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+    $_SESSION['DBNAME'] = 'cotd2';
+    $_SESSION['V2'] = 'true';
+    $_SESSION['DB'] = 'true';
+} else if ( !empty(getenv('DBHOST')) ) {
+    $_SESSION['V3'] = 'true';
+    $_SESSION['DB'] = 'true';
+}
+
 // Determine active theme default to cats                                                                                                         
+$_SESSION['selector'] = 'cats';
 if ( $selector = getenv('SELECTOR') ) {                                                                                                                     
     $_SESSION['selector'] = $selector;                                                                                                                      
 } elseif ( file_exists($ini_file) ) {                                                                                                                       
     $ini_array = parse_ini_file($ini_file);                                                                                                                 
     $_SESSION['selector'] = $ini_array['selector'];                                                                                                         
-} elseif ( ! empty($db) ) {
+} elseif ( $_SESSION['V2'] == 'true' )  {
     $_SESSION['selector'] = 'pets'; 
-} else {                                                                                                                                                    
-    $_SESSION['selector'] = 'cats';                                                                                                                         
 }                                        
 
 // Populate theme using local file or remote REST Service or DB
 if ( $service = getenv('SERVICE') ) {                                                                                                                     
     include('data/rest.php');
-} elseif ( ! empty($db) ) {
+} elseif ( $_SESSION['DB'] == 'true' ) {
     include('data/db.php');
 } else {
     include('data/'.$_SESSION['selector'].'.php');
